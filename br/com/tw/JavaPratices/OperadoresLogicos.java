8ae0039f7d2787ee8cc178a47895187ea14fe534
Your Name <joaohenrique2@hotmail.com>
2024-03-13 00:28:41 -0300

    OperadoresLogicos

--- br/com/tw/JavaPratices/OperadoresLogicos.java
+++ br/com/tw/JavaPratices/OperadoresLogicos.java
@@ -4,7 +4,7 @@ public class OperadoresLogicos {
 
     public static void main(String[] args) {
         //Not | nao
-        System.out.println(!(10 != 10));
+        System.out.println(!(10 != 11));
         
         //and | E
         System.out.println(10 > 20 && 10 > 5);
