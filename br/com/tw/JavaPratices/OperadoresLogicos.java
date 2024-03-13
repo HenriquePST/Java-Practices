55b8852fd06de78451535dbef16b63fcbd021248
Your Name <joaohenrique2@hotmail.com>
2024-03-13 00:03:13 -0300

    OperadoresLogicos

---
+++ br/com/tw/JavaPratices/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package br.com.tw.JavaPratices;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        //Not | nao
+        System.out.println(10 == 10 );
+        
+        //and | E
+        System.out.println(10> 20 && 10>5);
+    }
+    
+}
