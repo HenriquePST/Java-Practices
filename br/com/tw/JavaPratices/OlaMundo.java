ea06dc8d47c610d96cce5405345226e2419385da
Your Name <joaohenrique2@hotmail.com>
2024-03-13 20:40:41 -0300

    moved folder fundamentals

---
+++ br/com/tw/JavaPratices/OlaMundo.java
@@ -0,0 +1,7 @@
+package br.com.tw.JavaPratices;
+
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.println("OlaMundo");
+}
+}
---
+++ br/com/tw/JavaPratices/OperadoresLogicos.java
@@ -0,0 +1,16 @@
+package br.com.tw.JavaPratices;
+
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        //Not | nao
+        System.out.println(!(10 != 11));
+        
+        //and | E
+        System.out.println(10 > 20 && 10 > 5);
+        
+        // or | Ou
+        System.out.println(10 <20 || 10 < 5);
+    }
+    
+}
