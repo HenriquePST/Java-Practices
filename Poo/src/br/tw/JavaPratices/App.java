65abf0b54113761dd23ad62b1ba83b7e63125225
Your Name <joaohenrique2@hotmail.com>
2024-03-13 22:47:59 -0300

    Poo - Metodos e Construtores

--- Poo/src/br/tw/JavaPratices/App.java
+++ Poo/src/br/tw/JavaPratices/App.java
@@ -5,22 +5,23 @@ import Poo.src.br.tw.JavaPratices.classes.Diarista;
 
 public class App {
     public static void main(String[] args) {
-        var joao = new Diarista();
-        joao.nome = "Joao Candido";
-        joao.telefone = "81 9999999";
-        joao.endereco = "av. recife";
-
+        var joao = new Diarista("Joao Candido","81 9999999","av. recife");
+      
         System.out.println("nome" + joao.nome);
         System.out.println("telefone" + joao.telefone);
         System.out.println("endereco" + joao.endereco);
 
-        var Maria = new Diarista();
-        Maria.nome = "Maria da Silva";
-        Maria.telefone = "81 99999";
-        Maria.endereco = "av.recife";
+        joao.atender("Joao");
 
+        var Maria = new Diarista("Maria da Silva","81 99999","av.recife");
+       
         System.out.println("nome" + Maria.nome);
         System.out.println("telefone" + Maria.telefone);
         System.out.println("endereco" + Maria.endereco);
+
+        Maria.atender("maria");
+
+        var Luiz = new Diarista("Luiz");
+        System.out.println("nome" + Luiz.nome);
     }
 }
--- Poo/src/br/tw/JavaPratices/classes/Diarista.java
+++ Poo/src/br/tw/JavaPratices/classes/Diarista.java
@@ -5,5 +5,18 @@ public class Diarista {
     public String nome;
     public String telefone;
     public String endereco;
+    
+    public Diarista(String nome){
+        this.nome = nome;
+    }
+    public Diarista(String nome, String telefone, String endereco){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+    // metodos 
 
+    public void atender(String nomeCliente){
+        System.out.println("realizando atendimento para cliente" + nomeCliente);
+    }
 }
