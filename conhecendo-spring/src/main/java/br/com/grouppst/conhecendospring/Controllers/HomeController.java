b6d3ded572a2b54a29e0312227880d10ca64fe6c
Your Name <joaohenrique2@hotmail.com>
2024-03-17 17:23:54 -0300

    Passando informações do Controller para a View

--- conhecendo-spring/src/main/java/br/com/grouppst/conhecendospring/Controllers/HomeController.java
+++ conhecendo-spring/src/main/java/br/com/grouppst/conhecendospring/Controllers/HomeController.java
@@ -1,6 +1,8 @@
 package br.com.grouppst.conhecendospring.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,6 +13,12 @@ public class HomeController {
         @RequestMapping (name = "/", method = RequestMethod.GET)
         public String home() {
             return "home1";
+        }
+
+        @GetMapping("/modelmap")
+        public String home2(ModelMap modelMap) {
+            modelMap.addAttribute("nome", "Spring");
+            return "home";
         }
      }
 
