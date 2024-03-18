55691e1b9a2846cc06444b05cb1f7cb239b69f5f
Your Name <joaohenrique2@hotmail.com>
2024-03-17 21:16:46 -0300

    Criando projeto Spring Boot com Spring Initializr

---
+++ conhecendospring/src/main/java/br/com/grouppst/conhecendospring/ConhecendospringApplication.java
@@ -0,0 +1,13 @@
+package br.com.grouppst.conhecendospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConhecendospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConhecendospringApplication.class, args);
+	}
+
+}
