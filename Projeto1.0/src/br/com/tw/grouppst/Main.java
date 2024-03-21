4faa1839fad5122429a6513e94a7c25afad031a1
Your Name <joaohenrique2@hotmail.com>
2024-03-20 23:59:45 -0300

    Criando as classes da camada model

--- Projeto1.0/src/br/com/tw/grouppst/Main.java
+++ Projeto1.0/src/br/com/tw/grouppst/Main.java
@@ -1,7 +1,15 @@
 package br.com.tw.grouppst;
 
+import br.com.tw.grouppst.Model.Musico;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var musico = new Musico(1L,"Joao","81999999", "10210310422","av.caxanga");
+
+        System.out.println(musico.getId());
+        System.out.println(musico.getNome());
+        System.out.println(musico.getTelefone());
+        System.out.println(musico.getCpf());
+        System.out.println(musico.getEndereco());
     }
 }
---
+++ Projeto1.0/src/br/com/tw/grouppst/Model/Musico.java
@@ -0,0 +1,10 @@
+package br.com.tw.grouppst.Model;
+
+public class  Musico extends Pessoa {
+
+    public Musico(){}
+
+    public Musico(Long id, String nome, String telefone, String cpf, String endereco) {
+        super(id, nome, telefone, cpf, endereco);
+    }
+}
