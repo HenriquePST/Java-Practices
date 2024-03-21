cd859dec997a09bfa8d3681bc9e8a8a6bf8c8ec5
Your Name <joaohenrique2@hotmail.com>
2024-03-20 23:52:12 -0300

    Projeto 1.0

---
+++ Projeto1.0/src/br/com/tw/grouppst/Model/Pessoa.java
@@ -0,0 +1,88 @@
+package br.com.tw.grouppst.Model;
+
+import java.util.Objects;
+
+public abstract class Pessoa {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private String endereco;
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(Long id, String nome, String telefone, String cpf, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id) && Objects.equals(nome, pessoa.nome) && Objects.equals(telefone, pessoa.telefone) && Objects.equals(cpf, pessoa.cpf) && Objects.equals(endereco, pessoa.endereco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, telefone, cpf, endereco);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+}
