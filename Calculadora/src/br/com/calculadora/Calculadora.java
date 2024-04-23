8a4a69815be9ab75c26adf1dba73f854dc5a38a5
Your Name <joaohenrique2@hotmail.com>
2024-04-23 20:53:14 -0300

    "" Static e Instanciação de Objetos"

--- Calculadora/src/br/com/calculadora/Calculadora.java
+++ Calculadora/src/br/com/calculadora/Calculadora.java
@@ -6,33 +6,33 @@ public class Calculadora {
      * Declaração de variáveis de classe
      */
 
-    static int x, y;
-    static int soma, multiplicacao, divisao, subtracao;
+    int x, y;
+    int soma, multiplicacao, divisao, subtracao;
 
-    public static void soma() {
+    public  void soma() {
 
         soma = x + y;
         System.out.println(soma);
     }
 
-    public static int soma(int x, int y){
+    public  int soma(int x, int y){
         soma = x + y;
         System.out.println(soma);
         return soma;
     }
 
-    public static void subtracao() {
+    public void subtracao() {
         subtracao = x - y;
         System.out.println(subtracao);
     }
 
-    public static void Multiplicacao() {
+    public  void Multiplicacao() {
 
         multiplicacao = x * y;
         System.out.println(multiplicacao);
     }
 
-    public static void divisao() {
+    public  void divisao() {
         divisao = x / y;
         System.out.println(divisao);
 
--- Calculadora/src/br/com/calculadora/Main.java
+++ Calculadora/src/br/com/calculadora/Main.java
@@ -5,23 +5,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        int x,y;
+        /**
+         * Declaração de variáveis locais (main)
+         */
         Scanner scanner = new Scanner(System.in);
 
+        //Instanciação de objetos
+        Calculadora calc = new Calculadora();
+        Calculadora calc2 = new Calculadora();
+
+        /**
+         * Entrada de Dados
+         */
+
         System.out.println("digite o valor de x:");
-        Calculadora.x = scanner.nextInt();
+        calc.x = scanner.nextInt();
         System.out.println("digite o valor de y:");
-        Calculadora.y = scanner.nextInt();
+        calc.y = scanner.nextInt();
 
+        System.out.println("digite o valor de x para calc2");
+        calc2 x = scanner.nextInt();
+        System.out.println("digite o valor de y para calc2");
+        calc2.y = scanner.nextInt();
 
-        Calculadora.soma();
-        int MainCalcula = Calculadora.soma(40,20);
-        System.out.println(MainCalcula);
-        Calculadora.subtracao();
-        Calculadora.Multiplicacao();
-        Calculadora.divisao();
+        calc.soma();
+        calc.subtracao();
+        calc.Multiplicacao();
+        calc.divisao();
 
+        System.out.println("-");
+        System.out.println("====CAL2=====");
+        calc2.soma();
+        calc2.subtracao();
+        calc2.Multiplicacao();
+        calc2.divisao();
 
     }
 }
