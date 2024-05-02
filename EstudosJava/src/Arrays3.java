3e8bf3295a58e5c31e55d1b8976fa52f4a65008e
Your Name <joaohenrique2@hotmail.com>
2024-05-01 23:52:05 -0300

    "foreach"

--- EstudosJava/src/Arrays3.java
+++ EstudosJava/src/Arrays3.java
@@ -2,7 +2,7 @@ public class Arrays3 {
 
     //foreach
     public static void main(String[] args) {
-        String[] nomes = {"Juba","Ju","Jo"};
+        String[] nomes = {"Juba","Juh","Jo"};
 
         for (String nome : nomes){
         System.out.println(nome);
