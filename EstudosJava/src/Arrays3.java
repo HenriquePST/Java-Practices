a67e9efa88268d5a1f2ccfdd9fbdd72a6a0060c5
Your Name <joaohenrique2@hotmail.com>
2024-05-01 23:42:46 -0300

    "bidimensional"

---
+++ EstudosJava/src/Arrays3.java
@@ -0,0 +1,12 @@
+public class Arrays3 {
+
+    //foreach
+    public static void main(String[] args) {
+        String[] nomes = {"Juba","Ju","Jo"};
+
+        for (String nome : nomes){
+        System.out.println(nome);
+      }
+
+    }
+}
