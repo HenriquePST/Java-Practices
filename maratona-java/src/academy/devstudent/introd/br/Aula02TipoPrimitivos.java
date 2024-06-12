7e9db21ae3bd9329ae1be0dfe4a137a1a5ac1497
Your Name <joaohenrique2@hotmail.com>
2024-06-11 21:08:18 -0300

    declaracao e tamanho em memoria

--- maratona-java/src/academy/devstudent/introd/br/Aula02TipoPrimitivos.java
+++ maratona-java/src/academy/devstudent/introd/br/Aula02TipoPrimitivos.java
@@ -2,6 +2,21 @@ package academy.devstudent.introd.br;
 
 public class Aula02TipoPrimitivos {
     public static void main(String[] args) {
-        
+        // int, double, float , char, byte, short, long, boolean
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdade = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+       System.out.println("A idade é "+idade+ " anos");
+        System.out.println(falso);
+        System.out.println(caractere+"char");
+
+
     }
 }
