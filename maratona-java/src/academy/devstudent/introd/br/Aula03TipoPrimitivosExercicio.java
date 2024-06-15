1630ed9b4d4a5826011a13da6784e38c0832f7eb
Your Name <joaohenrique2@hotmail.com>
2024-06-14 21:52:54 -0300

    Exercicio Tipo Primitivos

---
+++ maratona-java/src/academy/devstudent/introd/br/Aula03TipoPrimitivosExercicio.java
@@ -0,0 +1,14 @@
+package academy.devstudent.introd.br;
+
+/*
+Crie variaveis para os cmpos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>
+confirmo que recebi o salario de <salario>, na da <data>
+
+ */
+public class Aula03TipoPrimitivosExercicio {
+    public static void main(String[] args) {
+
+    }
+}
